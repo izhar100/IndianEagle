a42547a6683da3ba531228b630035f6559881932
izhar100 <ezharashraf523@gmail.com>
2023-01-19 00:31:22 +0530

    home page

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -10,3 +10,9 @@ let divimages=[
     "https://assets.olapic-cdn.com/media/j/d/n/jdnxs65/normal.jpg",
     "https://assets.olapic-cdn.com/media/a/p/i/apixs65/normal.jpg",
    ]
+
+   const searchbox=document.querySelector("#searchinp")
+   const searchbtn=document.querySelector("#searchbtn")
+   searchbtn.addEventListener("click",()=>{
+    console.log(searchbox.value)
+   })

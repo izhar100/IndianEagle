9956ce5865bc379520a45c5b8019d6da461744b8
izhar100 <ezharashraf523@gmail.com>
2023-01-18 22:06:53 +0530

    home page

---
+++ homepage/homepage.js
@@ -0,0 +1,12 @@
+let divimages=[
+    "https://assets.olapic-cdn.com/media/c/p/i/cpixs65/normal.jpg",
+    "https://assets.olapic-cdn.com/media/5/u/n/5unxs65/normal.jpg",
+    "https://assets.olapic-cdn.com/media/c/p/i/cpixs65/normal.jpg",
+    "https://assets.olapic-cdn.com/media/n/z/m/nzmxs65/normal.jpg",
+    "https://assets.olapic-cdn.com/media/j/d/n/jdnxs65/normal.jpg",
+    "https://assets.olapic-cdn.com/media/a/p/i/apixs65/normal.jpg",
+    "https://assets.olapic-cdn.com/media/5/u/n/5unxs65/normal.jpg",
+    "https://assets.olapic-cdn.com/media/n/z/m/nzmxs65/normal.jpg",
+    "https://assets.olapic-cdn.com/media/j/d/n/jdnxs65/normal.jpg",
+    "https://assets.olapic-cdn.com/media/a/p/i/apixs65/normal.jpg",
+   ]
